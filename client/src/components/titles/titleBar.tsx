1cf924980b0d8c8d77d51bd661380ebe5170516a
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-04 15:26:52 -0400

    styles: change title color

--- client/src/components/titles/titleBar.tsx
+++ client/src/components/titles/titleBar.tsx
@@ -5,8 +5,8 @@ interface TitleBarProps {
 export default function TitleBar({ title }: TitleBarProps) {
     return (
         <div className="w-fit h-fit p-3 flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-semibold text-blue-800 text-center">{title}</h1>
-            <div className="w-full h-1 bg-blue-800 rounded-full"></div>
+            <h1 className="text-2xl font-semibold text-blue-700 text-center">{title}</h1>
+            <div className="w-full h-1 bg-blue-700 rounded-full"></div>
         </div>
     )
 }

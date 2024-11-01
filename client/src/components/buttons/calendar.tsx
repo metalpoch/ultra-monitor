b77bb47ff7a3171f57e8292e661da9ce869fa95f
Angxandralol <angyeemarin.dev@gmail.com>
2024-11-01 08:45:57 -0400

    fix: bad border

--- client/src/components/buttons/calendar.tsx
+++ client/src/components/buttons/calendar.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 export default function CalendarSelector(content: Props) {
     return (
-        <div className="h-8 flex flex-row md:w-full max-sm:h-fit max-sm:flex-col">
-            <label id={content.id} className="w-fit h-full px-4 py-1 bg-blue-700 text-white font-bold max-sm:w-full md:rounded-l-full max-sm:rounded-t-xl">{content.label}</label>
+        <div className="h-8 flex flex-row min-w-fit max-md:w-full max-md:h-fit max-md:flex-col">
+            <label id={content.id} className="w-fit h-full px-4 py-1 bg-blue-700 text-white font-bold max-md:w-full md:rounded-l-full max-md:rounded-t-xl">{content.label}</label>
             <input id={content.id} type="date" className="w-full h-full bg-gray-55 px-2 max-sm:w-full md:rounded-r-full max-sm:rounded-b-xl" />
         </div>
     );

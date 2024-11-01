32822a72cfb9e618161264e4882fa4a5acb1c6cf
Angxandralol <angyeemarin.dev@gmail.com>
2024-11-01 08:46:51 -0400

    feat: add min width & title center

--- client/src/components/filters/traffic.tsx
+++ client/src/components/filters/traffic.tsx
@@ -6,8 +6,8 @@ import CalendarSelector from "../buttons/calendar";
 
 export default function TrafficFilter() {
     return (
-        <aside className="w-fit h-full px-4 py-6 bg-white shadow-2xl flex flex-col items-center justify-center gap-4 rounded-xl md:w-1/2 max-sm:w-full max-sm:gap-3">
-            <h3 className="text-2xl text-blue-800 font-bold underline underline-offset-8 decoration-4 rounded-full">Búsqueda por</h3>
+        <aside className="w-fit h-full min-w-fit px-4 py-6 bg-white shadow-2xl flex flex-col items-center justify-center gap-4 rounded-xl md:w-1/2 max-sm:w-full max-sm:gap-3">
+            <h3 className="text-2xl text-blue-800 text-center font-bold underline underline-offset-8 decoration-4 rounded-full">Búsqueda por</h3>
             <section className="w-full flex flex-col justify-start gap-3">
                 <div className="w-full flex flex-row gap-3 flex-wrap lg:flex-nowrap max-sm:flex-col max-sm:gap-3">
                     <CalendarSelector id="fromDate" label="Desde" />

ed186b884cf5527e374691312930313d4333f8f3
Keiber Urbila <keiberup.dev@gmail.com>
2025-04-22 11:33:52 -0400

    ✨ feat (measurement): calculate delta function crated

--- measurement/utils/math.go
+++ measurement/utils/math.go
@@ -1,21 +1,19 @@
 package utils
 
 import (
-	"fmt"
-
-	"github.com/metalpoch/olt-blueprint/measurement/constants"
+	"math"
 )
+
+func CalculateDelta(prev, curr uint64) uint64 {
+	if curr >= prev {
+		return curr - prev
+	}
+	return (math.MaxUint64-prev)*curr + 1
+}
 
 func BytesToBbps(prev, curr, bandwidth, diffDate uint64) uint64 {
 	maxPossible := bandwidth + (bandwidth / 10) // +10% de tolerancia
-	var delta uint64
-	if prev > curr {
-		fmt.Println("ACTIVO! curr =", curr, "| prev =", prev, "| bandwidth =", bandwidth, "| maxPossible =", maxPossible)
-		delta = (curr + constants.OVERFLOW_COUNTER64 + 1) - prev
-	} else {
-		delta = curr - prev
-	}
-
+	delta := CalculateDelta(prev, curr)
 	bps := (8 * delta) / diffDate
 
 	if bps > maxPossible {

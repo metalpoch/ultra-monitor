377632605e0047f11bfd261ee835fb423fb821d3
Angxandralol <angyeemarin.dev@gmail.com>
2025-04-25 13:19:14 -0400

    fix: upload reports

--- client/src/controllers/report.ts
+++ client/src/controllers/report.ts
@@ -5,12 +5,12 @@ export class ReportController {
      * Upload a file to the report.
      * 
      * @param {file} file File to be uploaded.
-     * @param {id} id ID of the file.
+     * @param {id} id User ID.
      * @param {category} category Category of the file.
      * @returns {boolean} True if the file was uploaded successfully, false otherwise.
      */
     static async uploadFile(file: any, id: number, category: string = "Reporte Regional"): Promise<boolean> {
-        const response = await ReportService.uploadFile(file[0], id, category);
+        const response = await ReportService.uploadFile(file, id, category);
         if ((response.status === 201) || (response.status === 200)) {
             return true;
         } else {

4875cad1cb58190397e69e5af782a07ac6060fe9
Keiber Urbila <keiberup.dev@gmail.com>
2025-06-06 11:56:27 -0400

    🐛fix (fat): replace constants by query variable

--- repository/fat.repository.go
+++ repository/fat.repository.go
@@ -5,12 +5,11 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	"github.com/metalpoch/ultra-monitor/entity"
-	"github.com/metalpoch/ultra-monitor/internal/constants"
 )
 
 type FatRepository interface {
 	AllFat(ctx context.Context, page, limit uint16) ([]entity.Fat, error)
-	AddFat(ctx context.Context, tao *entity.Fat) error
+	AddFat(ctx context.Context, fat *entity.Fat) error
 	DeleteOne(ctx context.Context, id int32) error
 	GetByID(ctx context.Context, id int32) (entity.Fat, error)
 	GetStates(ctx context.Context) ([]string, error)
@@ -32,23 +31,40 @@ func NewFatRepository(db *sqlx.DB) *fatRepository {
 func (repo *fatRepository) AllFat(ctx context.Context, page, limit uint16) ([]entity.Fat, error) {
 	var res []entity.Fat
 	offset := (page - 1) * limit
-	err := repo.db.SelectContext(ctx, &res, constants.SQL_SELECT_ALL_FATS, limit, offset)
+	query := `SELECT * FROM fats ORDER BY region, state, municipality, county LIMIT $1 OFFSET $2`
+	err := repo.db.SelectContext(ctx, &res, query, limit, offset)
 	return res, err
 }
 
-func (repo *fatRepository) AddFat(ctx context.Context, tao *entity.Fat) error {
-	_, err := repo.db.NamedExecContext(ctx, constants.SQL_INSERT_FAT, tao)
+func (repo *fatRepository) AddFat(ctx context.Context, fat *entity.Fat) error {
+	query := `
+    INSERT INTO fats (
+        fat, region, state, municipality, county, odn, olt_ip,
+        pon_shell, pon_port, pon_card, latitude, longitude
+    ) VALUES (
+        :fat, :region, :state, :municipality, :county, :odn, :olt_ip,
+        :pon_shell, :pon_port, :pon_card, :latitude, :longitude
+    )
+    ON CONFLICT (fat, state, municipality, county, olt_ip, odn, pon_shell, pon_card, pon_port)
+    DO UPDATE SET
+        region = EXCLUDED.region,
+        latitude = EXCLUDED.latitude,
+        longitude = EXCLUDED.longitude;`
+
+	_, err := repo.db.NamedExecContext(ctx, query, fat)
 	return err
 }
 
 func (repo *fatRepository) DeleteOne(ctx context.Context, id int32) error {
-	_, err := repo.db.ExecContext(ctx, constants.SQL_DELETE_FAT_BY_ID, id)
+	query := `DELETE FROM fats WHERE id = $1`
+	_, err := repo.db.ExecContext(ctx, query, id)
 	return err
 }
 
 func (repo *fatRepository) GetByID(ctx context.Context, id int32) (entity.Fat, error) {
 	var res entity.Fat
-	err := repo.db.GetContext(ctx, &res, constants.SQL_SELECT_FAT_BY_ID, id)
+	query := `SELECT * FROM fats WHERE id = $1`
+	err := repo.db.GetContext(ctx, &res, query, id)
 	return res, err
 }
 

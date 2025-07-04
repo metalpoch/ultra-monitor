d862e33d2b332ec6dd7b6ec64cb488b6606a227a
Keiber Urbila <keiberup.dev@gmail.com>
2025-07-04 15:08:15 -0400

    🐛fix (server): dto field required

--- internal/dto/fat.go
+++ internal/dto/fat.go
@@ -3,18 +3,18 @@ package dto
 import "time"
 
 type Fat struct {
-	Fat          string    `json:"fat"`
-	Region       string    `json:"region"`
-	State        string    `json:"state"`
-	Municipality string    `json:"municipality"`
-	County       string    `json:"county"`
+	Fat          string    `json:"fat" validate:"required"`
+	Region       string    `json:"region" validate:"required"`
+	State        string    `json:"state" validate:"required"`
+	Municipality string    `json:"municipality" validate:"required"`
+	County       string    `json:"county" validate:"required"`
 	Odn          string    `json:"odn"`
-	OltIP        string    `json:"olt_ip"`
+	OltIP        string    `json:"olt_ip" validate:"required,ip_addr"`
 	Shell        uint8     `json:"shell"`
-	Card         uint8     `json:"card"`
-	Port         uint8     `json:"port"`
-	Actives      int       `json:"actives"`
-	Inactive     int       `json:"inactive"`
-	Others       int       `json:"others"`
-	ReportDay    time.Time `json:"report_day"`
+	Card         uint8     `json:"card" validate:"required"`
+	Port         uint8     `json:"port" validate:"required"`
+	Actives      int       `json:"actives" validate:"required"`
+	Inactive     int       `json:"inactive" validate:"required"`
+	Others       int       `json:"others" validate:"required"`
+	ReportDay    time.Time `json:"report_day" validate:"required"`
 }

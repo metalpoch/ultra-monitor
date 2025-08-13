1c84a01d0ebd220a461d8789e1f614e2eb3422d2
Keiber Urbila <keiberup.dev@gmail.com>
2025-08-13 13:44:58 -0400

    ✨ feat(web): 404 view created

--- routes/web.routes.go
+++ routes/web.routes.go
@@ -10,8 +10,14 @@ func NewWebRoutes(app *fiber.App, webAppDir string) {
 	app.Get("/", static.New(webAppDir+"/index.html"))
 	app.Get("/auth/login", static.New(webAppDir+"/auth/login"))
 	app.Get("/_astro/*", static.New(webAppDir+"/_astro/"))
-	app.Get("/404", static.New(webAppDir+"/404.html"))
 	app.Get("/health", func(c fiber.Ctx) error {
 		return c.SendString("OK")
+	})
+
+	app.Use(func(c fiber.Ctx) error {
+		if len(c.Path()) >= 4 && c.Path()[:4] == "/api" {
+			return c.Next()
+		}
+		return c.SendFile(webAppDir + "/404.html")
 	})
 }

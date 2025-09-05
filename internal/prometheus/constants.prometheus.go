ed27ae7d22f0cd6c06ec8e52c26f9381776f4895
Keiber Urbila <keiberup.dev@gmail.com>
2025-09-05 12:10:39 -0400

    🐛 fix(prometheus): resolve duplicate series error in OltStatsByState by making label set unique in PromQL queries

--- internal/prometheus/constants.prometheus.go
+++ internal/prometheus/constants.prometheus.go
@@ -4,11 +4,11 @@ const QUERY_STATES_BY_REGION_IN string = `sum(rate(hwGponOltEthernetStatisticRec
 const QUERY_STATES_BY_REGION_OUT string = `sum(rate(hwGponOltEthernetStatisticSendBytes_count{region="%s"}[15m]) * 8) by (state)`
 const QUERY_STATES_BY_REGION_IFSPEED string = `sum(ifSpeed{region="%s"}) by (state)`
 
-const QUERY_OLT_BY_STATES_IN string = `sum(rate(hwGponOltEthernetStatisticReceivedBytes_count{state="%s"}[15m]) * 8) by (instance)
-	* on (instance) group_left(sysName) sysName`
-const QUERY_OLT_BY_STATES_OUT string = `sum(rate(hwGponOltEthernetStatisticSendBytes_count{state="%s"}[15m]) * 8) by (instance)
-	* on (instance) group_left(sysName) sysName`
-const QUERY_OLT_BY_STATES_IFSPEED string = `sum(ifSpeed{state="%s"}) BY (instance) * on (instance) group_left(sysName) sysName`
+const QUERY_OLT_BY_STATES_IN string = `sum(rate(hwGponOltEthernetStatisticReceivedBytes_count{state="%s"}[15m]) * 8) by (instance,job,state,region)
+	* on (instance,job,state,region) group_left(sysName) sysName`
+const QUERY_OLT_BY_STATES_OUT string = `sum(rate(hwGponOltEthernetStatisticSendBytes_count{state="%s"}[15m]) * 8) by (instance,job,state,region)
+	* on (instance,job,state,region) group_left(sysName) sysName`
+const QUERY_OLT_BY_STATES_IFSPEED string = `sum(ifSpeed{state="%s"}) BY (instance,job,state,region) * on (instance,job,state,region) group_left(sysName) sysName`
 
 const QUERY_GPON_STATS_IN string = `sum(rate(hwGponOltEthernetStatisticReceivedBytes_count{instance="%s"}[15m]) * 8) by (ponPortIndex)
   * on(ponPortIndex) group_left(ifName)

2a0b8f56852c28c652ffda15a17bc938aa833243
Keiber Urbila <keiberup.dev@gmail.com>
2025-09-29 16:51:37 -0400

    🐛 fix(server&admin view): routes fixed

--- routes/web.routes.go
+++ routes/web.routes.go
@@ -8,8 +8,17 @@ import (
 func NewWebRoutes(app *fiber.App, webAppDir string) {
 
 	app.Get("/", static.New(webAppDir+"/index.html"))
+
 	app.Get("/auth/login", static.New(webAppDir+"/auth/login"))
+
 	app.Get("/traffic", static.New(webAppDir+"/traffic"))
+	app.Get("/trend", static.New(webAppDir+"/trend"))
+	app.Get("/report", static.New(webAppDir+"/report"))
+
+	app.Get("/admin", static.New(webAppDir+"/admin"))
+	app.Get("/admin/user", static.New(webAppDir+"/admin/user"))
+	app.Get("/admin/fat", static.New(webAppDir+"/admin/fat"))
+	app.Get("/admin/ont", static.New(webAppDir+"/admin/ont"))
 
 	app.Get("/_astro/*", static.New(webAppDir+"/_astro/"))
 	app.Get("/health", func(c fiber.Ctx) error { return c.SendString("OK") })
